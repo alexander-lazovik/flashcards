d0be312409b19e2d80f68c60a7812a2367fadf09
Alexander Lazovik <alexander.lazovik@gmail.com>
2019-03-10 16:45:12 +0200

    Task 4: Added a test

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -6,10 +6,10 @@ describe Image do
     image2 = double("image")
     allow(image1).to receive(:id).and_return(1)
     allow(image1).to receive(:title).and_return('Test image 1')
-    allow(image1).to receive(:url).and_return('public/1.jpg')
+    allow(image1).to receive(:url).and_return('spec/fixtures/1.jpg')
     allow(image2).to receive(:id).and_return(2)
     allow(image2).to receive(:title).and_return('Test image 2')
-    allow(image2).to receive(:url).and_return('public/2.jpg')
+    allow(image2).to receive(:url).and_return('spec/fixtures/2.jpg')
 
     allow(Image).to receive(:search) { [image1, image2] }
   end
@@ -18,6 +18,17 @@ describe Image do
     images = Image.search('test')
     expect(images.count).to be 2
     expect(images[0].id).to be 1
-    expect(images[0].url).to eq('public/1.jpg')
+    expect(images[0].url).to eq('spec/fixtures/1.jpg')
+  end
+
+  it 'creates a new card with image' do
+    user = create(:user)
+    block =  create(:block, user: user)
+    images = Image.search('test')
+    image = Rails.root.join(images[0].url).open
+
+    card = Card.create(original_text: 'дом', translated_text: 'house',
+                       user_id: user.id, block_id: block.id, image: image)
+    expect(card.errors.any?).to be false
   end
 end
